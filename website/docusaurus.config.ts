0c90f9eb83107a364db615403a90279c7c9ba970
Liviu Ionescu <ilg@livius.net>
2023-10-30 08:54:28 +0200

    config.ts: update links to edit

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/cronica-it/preview/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/cronica-it/preview/edit/master/website/',
         },
         theme: {
           customCss: './src/css/custom.css',
