b06f7729f2021a1f3e2ca33686279c6a24659490
Liviu Ionescu <ilg@livius.net>
2023-12-30 18:08:56 +0200

    add components/ImagesTable

---
+++ website/src/components/ImagesTable/index.tsx
@@ -0,0 +1,35 @@
+// import logger from '@docusaurus/logger'
+
+const rowSize = 3
+
+function splitRows(images) {
+    const rows = []
+    for (let i = 0; i < images.length / rowSize; ++i) {
+        const row = []
+        for (let j = 0; j < rowSize; ++j) {
+            if (i * rowSize + j < images.length) {
+                row.push(images[i * rowSize + j])
+            }
+        }
+        rows.push(row)
+    }
+    return rows
+}
+
+function Columns({ columns }) {
+    return columns.map(column => <td><img src={column.src} alt={column.alt} /></td>)
+}
+
+function Rows({ rows }) {
+    return rows.map(row => <tr><Columns columns={row} /></tr>)
+}
+
+export function ImagesTable({ images }): JSX.Element {
+    const rows = splitRows(images)
+
+    return (
+        <table class="images">
+            <Rows rows={rows} />
+        </table>
+    );
+}
