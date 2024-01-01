ccfb0453da9ec3a98c31960b2e2ec37a8a09cb5a
Liviu Ionescu <ilg@livius.net>
2024-01-01 02:36:54 +0200

    scripts update

---
+++ scripts/update-date-from-event-date.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+file_path="$1"
+
+event_date=$(grep 'eventDate:' ${file_path} | sed -e "s|eventDate: '||" -e "s|'||")
+if $(echo ${event_date} | grep -q -v "-")
+then
+  event_date+="-07"
+fi
+echo $event_date
+
+sed -i.bak -e "s|date:|creationDate:|" -e "/creationDate:/ i\\
+date: $event_date" \
+${file_path} | head -n 10
+rm ${file_path}.bak
+
+# -----------------------------------------------------------------------------
+
