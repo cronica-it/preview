745709d1e8d1cd22d6efb831260db5c9bf7ba29e
Liviu Ionescu <ilg@livius.net>
2024-01-10 11:28:48 +0200

    components: add copyright

--- website/src/components/ImagesTable/index.tsx
+++ website/src/components/ImagesTable/index.tsx
@@ -1,3 +1,14 @@
+/*
+ * This file is part of the Cronica-IT project (https://github.com/cronica-it).
+ * Copyright (c) 2023 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/license/mit/.
+ */
+
 // import logger from '@docusaurus/logger'
 
 const rowSize = 3
